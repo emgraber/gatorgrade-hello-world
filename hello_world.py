d4693288518c4cf7a51971433cac9c5865538324
Lauren Nevill <nevill01@allegheny.edu>
2022-05-04 11:28:40 -0400

    Write sample source code

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,6 @@
-"""Create a Hello World! function to act as a GatorGrade example."""
+"""Create a hello world function to act as a GatorGrade example."""
+
+def hello_world(name):
+    print(f"Hello, {name}!")
+
+hello_world("gator")
