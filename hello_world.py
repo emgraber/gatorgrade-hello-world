e647b662556d13e4550979c3fc77905147eddc21
Maria Kim <yeeunmariakim@gmail.com>
2022-07-18 12:03:33 -0400

    Add Poetry and rich

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,16 @@
 """Create a hello world function to act as a GatorGrade example."""
+import rich
 
-def hello_world(name):
+
+def say_hello(name):
+    """Print a greeting."""
     print(f"Hello, {name}!")
 
-hello_world("gator")
+
+def say_hello_color(name, color):
+    """Print a greeting in color."""
+    rich.print(f"[{color}]Hello, {name}!")
+
+
+say_hello("gator")
+say_hello_color("gator", "blue")
