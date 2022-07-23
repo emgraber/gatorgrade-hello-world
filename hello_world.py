ef8f3a0984e56ac582837ad070180694b491d36f
Maria Kim <yeeunmariakim@gmail.com>
2022-07-23 14:43:43 -0400

    Use pylint as example shell check
    mdl not guaranteed to be installed; pylint can be run with Poetry.

--- hello_world.py
+++ hello_world.py
@@ -3,15 +3,23 @@ import rich
 
 
 def say_hello(name):
-    """Print a greeting."""
+    """Print a greeting.
+
+    Args:
+        name: The name to print in the greeting.
+    """
     print(f"Hello, {name}!")
 
 
 def say_hello_color(name, color):
-    """Print a greeting in color."""
+    """Print a greeting in color.
+
+    Args:
+        name: The name to print in the greeting.
+        color: The color to print the greeting in.
+    """
     rich.print(f"[{color}]Hello, {name}!")
 
 # TODO: Call say_hello
 
 # TODO: Call say_hello_color
-
